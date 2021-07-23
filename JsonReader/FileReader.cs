74d82c7e08f1f794bc2fe7bb82715fdccfe98964
Jitesh456 <jitesh0dubey@gmail.com>
2021-07-23 10:21:44 +0530

    ADDED:JsonFileReading

---
+++ JsonReader/FileReader.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace JsonReader
+{
+    public class FileReader
+    {
+        public List<Inventory> inventories = new List<Inventory>();
+        
+        public List<Inventory> GetInventry()
+        {
+            string text = File.ReadAllText("D:\\DemoConsoleApp\\DemoLab\\JsonReader\\JsonReader\\Data.json");
+            inventories = JsonConvert.DeserializeObject<List<Inventory>>(text);
+            return inventories;
+        }
+
+    }
+}
